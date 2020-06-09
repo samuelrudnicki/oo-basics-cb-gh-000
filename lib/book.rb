39293234a38a81629e197b77b84539756b24d1fe
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-09 17:23:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,10 @@
 class Book
 attr_accessor :title, :author, :page_count, :genre
-def initialize(title)
-  @title = title
-end
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+  
+  end
 end
